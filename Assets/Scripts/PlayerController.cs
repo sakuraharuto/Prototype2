0b2946fa72882dffd81107c2faca5fed63a70560
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-02-20 18:48:03 -0500

    two player local multiplayer input setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{   
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 moveInput;
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        //var p1 = PlayerInput.Instantiate(prefab: Player, controlScheme: "KeyboardLeft", device: Keyboard.current);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void OnMovement(InputValue value){
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void PlayerMove(){
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        rb2d.velocity = playerVelocity;
+    }
+}
